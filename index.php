63b0ea6ce3a86ef10ae179e82b8d390950a68b03
iptvWS <88012543+iptvWS@users.noreply.github.com>
2021-08-24 10:23:15 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -51,8 +51,8 @@ body {
 		window.open("utils.php?act=init", "frameDet");
 	}
 	function modificaDatos(id){
-		alert("utils.php?act=modificaDatos&id="+document.frameDet.document.getElementById("fila"+valRadio).cells[1].innerHTML+"&nomUser="+document.getElementById("nomUsuario").value+"&list="+document.getElementById("lista").value+"&usaLista=S"+"&idUsuario="+document.getElementById("idUsuario").value);
-		window.open("utils.php?act=modificaDatos&id="+document.frameDet.document.getElementById("fila"+valRadio).cells[1].innerHTML+"&nomUser="+document.getElementById("nomUsuario").value+"&list="+document.getElementById("lista").value+"&usaLista=S"+"&idUsuario="+document.getElementById("idUsuario").value, "frameDet");
+		Console.log("utils.php?act=modificaDatos&id="+document.frameDet.document.getElementById("fila"+valRadio).cells[1].innerHTML+"&nomUser="+document.getElementById("nomUsuario").value+"&usaLista=S"+"&idUsuario="+document.getElementById("idUsuario").value+"&list="+document.getElementById("lista").value);
+		window.open("utils.php?act=modificaDatos&id="+document.frameDet.document.getElementById("fila"+valRadio).cells[1].innerHTML+"&nomUser="+document.getElementById("nomUsuario").value+"&usaLista=S"+"&idUsuario="+document.getElementById("idUsuario").value+"&list="+document.getElementById("lista").value, "frameDet");
 		alert("Permisos modificados");
 		window.open("utils.php?act=init", "frameDet");
 	}
