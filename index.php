6baf710701aa313cd243ac678067e3bf9db1df82
iptvWS <88012543+iptvWS@users.noreply.github.com>
2021-08-24 09:35:05 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,458 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+<meta charset="ISO-8859-1">
+<title>it's TV</title>
+ <link rel="shortcut icon" href="itsTV.png" />
+
+<style type="text/css">
+body {
+  background: url("mybackgroundimage.gif") repeat;
+}
+#banner {
+  position: fixed;
+  top: 0px;
+  left: 0px;
+  right: 0px;
+  width: 100%;
+  height: 170px;
+  z-index: 1;
+   background-color:#330b2d;
+  opacity: 0.9;
+}
+#div_lateral{
+  position: fixed;
+  z-index: 0; /*Crea una capa nueva por encima, si tenemos una con valor 2 estará a una altura o por encima de una con 
+                valor 1*/
+  
+  top: 170px;
+  left: 0px;
+  right: 0px;
+
+   height: 68%;
+   width: 170px;
+   background-image: url('');
+   background-position: left;
+   background-repeat: no-repeat;
+   background-size: cover;
+     background-size: 100% 100%;
+  background-color:#330b2d;
+  opacity: 0.9;
+  margin-left:0px; /*Con este margen posicionamos el div donde queramos*/
+}
+</style>
+<script data-ad-client="ca-pub-9702888902505497" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+<script type="text/javascript">
+	var valRadio=-1;
+	function modificaPermisos(id,valor){
+		window.open("utils.php?act=modifica&valor="+valor+"&id="+document.frameDet.document.getElementById("fila"+id).cells[1].innerHTML, "frameDet");
+		alert("Permisos modificados");
+		window.open("utils.php?act=init", "frameDet");
+	}
+	function modificaDatos(){
+		window.open("utils.php?act=modificaDatos&id="+document.frameDet.document.getElementById("fila"+id).cells[1].innerHTML+"&nomUser="+document.getElementById("nomUsuario").value+"&list="+document.getElementById("lista").value+"&usaLista=S"+"&idUsuario="+document.getElementById("idUsuario").value, "frameDet");
+		alert("Permisos modificados");
+		window.open("utils.php?act=init", "frameDet");
+	}
+	function selID(id){
+		alert("Funcionalidad no disponible");
+	}
+	function setRadio(id){
+		valRadio=id;
+	}
+	function abrirModifica() { //CASF.[7.1.1]
+		alert("_numero_"+document.getElementById("detalleUsuarios").rows[valRadio].cells[1].innerHTML+"_nom_"+document.getElementById("detalleUsuarios").rows[valRadio].cells[2].innerHTML+"_idD_"+document.getElementById("detalleUsuarios").rows[valRadio].cells[3].innerHTML);
+		
+		document.getElementById("idUsuario").value=document.getElementById("detalleUsuarios").rows[valRadio].cells[2].innerHTML;
+		document.getElementById("nomUsuario").value=document.getElementById("detalleUsuarios").rows[valRadio].cells[3].innerHTML;
+		document.getElementById("lista").value=document.getElementById("detalleUsuarios").rows[valRadio].cells[7].innerHTML;
+		setSizePantalla();
+		setCentro();
+		
+		document.getElementById("idModifica").style.width = xW + 'px';
+		document.getElementById("idModifica").style.height = yH + 'px';
+		document.getElementById("idModifica").style.backgroud = '#11ffee00';
+		document.getElementById("idModifica").style.opacity = '0.1';
+		document.getElementById("idModifica").style.visibility = 'visible';
+		
+		document.getElementById("modifica").style.left = xCentro - (500)+"px";  
+		document.getElementById("modifica").style.top  = yCentro - (25)+"px"; 
+		document.getElementById("modifica").style.display = '';
+		document.getElementById("idModifica").style.opacity = '0.9';
+		document.getElementById("idModifica").style.visibility = 'visible';
+	}
+	function cierraDiv(){
+		document.getElementById("idModifica").style.visibility = 'hidden';
+		document.getElementById("idModifica").style.visibility = 'hidden';
+	}
+	function setCentro(){//CASF.[7.1.1]
+		if (window.innerWidth){ 
+			alert("if"+window.innerWidth+"_"+window.innerHeight);
+			setCentroXY(parseInt(window.innerWidth / 2), parseInt(window.innerHeight / 2));
+		} else if(document.body.clientWidth){
+		alert("if else");
+			setCentroXY(parseInt(document.body.clientWidth / 2), parseInt(document.body.clientHeight / 2));
+		}
+	}	
+	function setCentroXY(x, y){//CASF.[7.1.1]
+		xCentro = x;
+		yCentro = y;
+	}
+	function setSizePantalla() { //CASF.[7.1.1]
+		if (window.innerWidth){
+			xW = window.innerWidth;
+			yH = window.innerHeight;
+		} else if(document.body.clientWidth){
+			xW = document.body.clientWidth;
+			yH = document.body.clientHeight;
+		}
+	} /** setSizePantalla */
+</script>
+</head>
+<body   style="background-image: url('');   background-position: left;
+   background-repeat: no-repeat;
+   background-size: cover;
+   background-color:#330b2d;
+     background-size: 100% 100%;text">
+
+
+<font color="white">
+
+<img id="banner" src="WTSPlay.jpg" style="" alt=""/>
+<div id="div_lateral"  >
+			 	<table >
+			 	<tr height="50px">
+			 			<td ><td>
+			 		</tr>
+			 		<tr height="50px">
+			 			<td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#desarrollador" style="color: red">Bloqueo de Usuarios</a><td>
+			 		</tr>
+			 		<tr height="50px">
+			 			<td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a onclick="abrirModifica()" style="color: blue">Edici�n</a><td>
+			 		</tr>
+			 		<tr height="50px">
+			 			<td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#CSTV2" style="color: red"></a><td>
+			 		</tr>
+			 		<tr height="50px">
+			 			<td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#CSTV" style="color: blue"></a><td>
+			 		</tr>
+			 		<tr height="50px">
+			 			<td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#CONTACTO" style="color: red"></a><td>
+			 		</tr>
+			 		<tr height="10px">
+			 			<td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="#CONTACTO"  style="color: blue"></a>
+			 		<tr height="10px" align="right">
+			 			<td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a   style="color: red">
+	 		
+	&nbsp;&nbsp;
+	</a>
+	
+	<td>	</tr>	 		
+			 	</table>
+			 </div>
+<table  >
+		<tr height="200px"   >
+			<td   width="220px"  >
+			
+			</td>
+			<td   width="220px"  >
+			
+			</td>
+			<td   width="220px"  >
+			
+			</td>
+			
+		</tr>
+		
+		<tr height="20%" >
+			
+			
+		</tr>
+		
+		<tr height="80%"  >
+			<td   width="220px"  >
+			
+			</td>
+			<td colspan="2"   >
+				    <h1>Mantenimiento y Bloqueo de Dispositivos</h1>
+    				<p>Seleccione el registro para editar o seleccione estatus de bloqueo</p>
+					
+			
+				</td>
+		</tr>
+		
+		
+		
+		<tr height="80%" >
+			<td   width="220px"  >
+			 
+			</td>
+			<td colspan="2" id="desarrollador" width="80%">
+			<!-- <iframe width="100%" height="500px" id="frameDet" name="frameDet" src="utils.php?act=init" style="border: none;" >
+				<font color="white">
+				</font>
+			</iframe> -->
+				    
+				    <table id ="detalleUsuarios" cellpadding="1" cellspacing="1" border="1" style="border: 1px solid white;" > 
+				    	<tr id="encabezado" style="border-width: 2px" id="fila0">
+				    		<td >Sel </td>
+				    		<td > ID Canal </td>
+				    		<td > ID Usuario </td>
+				    		<td > Usuario </td>
+				    		<td > ID de dispositivo </td>
+				    		<td > IP de conexi�n </td>
+				    		
+				    		<td > Permisos </td>
+				    		<td id="cdLista" style="display:none ; width: 0px" width="0px" >http://IP:PUERTO1/ </td>
+				    	</tr>
+				    	<tr style="border-width: 2px" id="fila1">
+				    	<td ><input id="radioSel" value="1" onclick="setRadio(this.value)"
+										name="radioSel"  type="radio"
+										> </td>
+				    		<td >1 </td>
+				    		<td >Usuario1 </td>
+				    		<td >PANCHO ROBLES </td>
+				    		<td >123456 </td>
+				    		<td >10.123.12.01 </td>
+				    		
+				    		<td >Permisos: <select>
+				    				<option>S</option>
+				    				<option>N</option>
+				    			  </select> 
+				    		</td>
+				    		<td id="cdLista" style="display:none ;" >http://IP:PUERTO2/ </td>
+				    	</tr>
+				    	<tr  style="border-width: 2px" id="fila2">
+				    	<td ><input id="radioSel" value="2" onclick="setRadio(this.value)"
+										name="radioSel"  type="radio"
+										> </td>
+				    		<td >2 </td>
+				    		<td >Usuario2 </td>
+				    		<td >PANCHO ROBLES 2</td>
+				    		<td >123456 </td>
+				    		<td >10.123.12.01 </td>
+				    		
+				    		<td >Permisos: <select>
+				    				<option>S</option>
+				    				<option>N</option>
+				    			  </select> 
+				    		</td>
+				    		<td id="cdLista" style="display:none ;" width="0px" >http://IP:PUERTO3/ </td>
+				    	</tr>
+				    	<tr id="encabezado" style="border-width: 2px" id="fila3">
+				    	<td ><input id="radioSel" value="3" onclick="setRadio(this.value)"
+										name="radioSel"  type="radio"
+										> </td>
+				    		<td >3 </td>
+				    		<td >Usuario3 </td>
+				    		<td >PANCHO ROBLES 3</td>
+				    		<td >123456 </td>
+				    		<td >10.123.12.01 </td>
+				    		
+				    		<td >Permisos: <select>
+				    				<option>S</option>
+				    				<option>N</option>
+				    			  </select> 
+				    		</td>
+				    		<td id="cdLista" style="display:none ;" width="0px" >http://IP:PUERTO4/ </td>
+				    	</tr>
+				    	<tr id="encabezado" style="border-width: 2px" id="fila4">
+				    	<td ><input id="radioSel" value="4" onclick="setRadio(this.value)"
+										name="radioSel"  type="radio"
+										> </td>
+				    		<td >4  </td>
+				    		<td >Usuario4 </td>
+				    		<td >PANCHO ROBLES 4</td>
+				    		<td >123456 </td>
+				    		<td >10.123.12.01 </td>
+
+				    		<td >Permisos: <select>
+				    				<option>S</option>
+				    				<option>N</option>
+				    			  </select> 
+				    		</td>
+				    	<td id="cdLista" style="display:none ;" width="0px" >http://IP:PUERTO5/ </td>
+				    	</tr>
+				    	<tr id="encabezado" style="border-width: 2px" id="fila5">
+				    	<td ><input id="radioSel" value="5" onclick="setRadio(this.value)"
+										name="radioSel"  type="radio"
+										> </td>
+				    		<td >5 </td>
+				    		<td >Usuario5 </td>
+				    		<td >PANCHO ROBLES 5</td>
+				    		<td >123456 </td>
+				    		<td >10.123.12.01 </td>
+				    	
+				    		<td >Permisos: <select id="5" onchange='modificaPermisos (this.id, this.value);'>
+				    				<option >S</option>
+				    				<option >N</option>
+				    			  </select> 
+				    		</td>
+				    	<td id="cdLista" style="display:none ;" width="0px"  >http://IP:PUERTO6/ </td>
+				    	</tr>
+				    	
+				    	
+				    	
+				    </table>
+
+				</td>
+		</tr>
+	
+		<tr height="80%" >
+			<td   width="220px"  >
+			
+			</td>
+			<td colspan="2" >
+				    <h1>Para recibir el canal </h1>
+				    <p>Para el instalador y apoyo es necesario que te unas a </p>
+    				<a href="https://t.me/cs_IPTV">Nuestro grupo de telegram</a>
+    				<p>Mandar tu captura de suscripcion al canal de youtube</p>
+    				<p>Y apoyar dando click en la publicidad</p>
+				<br>
+					<br>
+					<a  id="CONTACTO"></a>	
+				</td>
+		</tr>
+		<tr height="80%" >
+			
+			<td colspan="3">
+				    <td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>
+				</td>
+		</tr>
+		<tr height="80%" >
+			
+			<td colspan="3">
+				    <td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>
+				</td>
+		</tr>
+		
+		<tr height="80%" >
+		
+			<td colspan="3">
+				    <td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>
+				</td>
+		</tr>
+		<tr height="80%" >
+		
+			<td colspan="3">
+				    <td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>
+				</td>
+		</tr>
+		<tr height="80%" >
+		
+			<td colspan="3">
+				    <td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>
+				</td>
+		</tr>
+		<tr height="80%" >
+		
+			<td colspan="3">
+				    <td >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<td>
+				</td>
+		</tr>
+	</table>
+<!--   <div  class="barra-lateral" style="background-image: url('iptv13.png');  background-size:cover;"> -->
+    
+<!--   </div> -->
+<!--   <div class="contenido"> -->
+<!--     <h1>Otro tÃ­tuloâ¦</h1> -->
+<!--     <p>Otro texto</p> -->
+<!--     <p>MÃ¡s textoâ¦ </p> -->
+<!--   </div> -->
+</font>
+<font color="white">
+<div id="idModifica" style="left: 0; top: 0; position: absolute; visibility:hidden;background-color:#330b2d;" >
+<table border="0" >
+<tbody>	
+	<tr>
+		<td >
+			<div id="modifica" style=" left: 0; top: 0; position: absolute;border: 1px solid white;" >
+	  <table  border="0" cellpadding="1" cellspacing="0">
+		<tr>
+			<td colspan="5" style="align-content: center;" align="center">
+				Edici�n de usuario
+			</td>
+			
+		</tr>
+		<tr>
+			<td>
+			</td>
+			<td>
+			<td>
+			<td>
+			</td>
+			<td>
+			</td>
+			<td>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			</td>
+			<td>
+				ID Usuario:
+			<td>
+				<input type="text" size="30" maxlength="30" name="idUsuario" id="idUsuario" value="">
+			</td>
+			<td>
+			</td>
+			<td>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			</td>
+			<td>
+				Nombre Usuario:
+			<td>
+				<input type="text" size="70" maxlength="200" name="nomUsuario" id="nomUsuario" value="">
+			</td>
+			<td>
+			</td>
+			<td>
+			</td>
+		</tr>
+		<tr>
+			<td>
+			</td>
+			<td>
+				Lista:
+			<td>
+				<textarea  name="lista" size="200" cols="70	" rows="10" maxlength="200"  id="lista" >
+				http://IP:PORT/
+				</textarea>
+			</td>
+			<td>
+			</td>
+			<td>
+			</td>
+		</tr>
+		<tr align="center">
+			<td >
+			</td>
+			<td  >
+				
+			</td>
+			<td >
+			
+			<input type="button" onclick="alert('Aceptar');" value="Aceptar">
+			<input type="button" onclick="cierraDiv();" value="Cancelar">
+			</td>
+			<td >  
+				
+			</td>
+			<td >
+			</td>
+		</tr>
+	</table>
+				</div>
+		</td>
+	</tr>
+	
+</tbody>
+</table>
+</div>
+</font>
+</body>
+</html>
---
+++ utils.php
@@ -0,0 +1,363 @@
+<?php
+$accion = $_GET["act"]; // le faltaba esta instrucciï¿½n antes del if
+$uid = $_GET["uid"]; // le faltaba esta instrucciï¿½n antes del if
+$id = $_GET["id"]; // le faltaba esta instrucciï¿½n antes del if
+$lista = $_GET["list"]; // le faltaba esta instrucciï¿½n antes del if
+$valor = $_GET["valor"]; // le faltaba esta instrucciï¿½n antes del if
+$nomUser = $_GET["nomUser"]; // le faltaba esta instrucciï¿½n antes del if
+$idUsuario = $_GET["idUsuario"]; // le faltaba esta instrucciï¿½n antes del if
+$usaLista = $_GET["usaLista"]; // le faltaba esta instrucciï¿½n antes del if
+
+
+
+
+
+
+if($accion=="init"){
+    // "Definiciï¿½n" de la base de datos
+    $conexion = null;
+    $conexion = conectar_PostgreSQL("hyssxqqsenyzws", "f7393c156cc3f0fd9aa4c73673142c54a98a837bdf43fa73dc2392b55b176719"
+        ,  "ec2-52-0-67-144.compute-1.amazonaws.com"
+        , "dbreil1d2te41c");
+    //listarPersonas($conexion, $id);
+    
+    $salida= obtieneRegistros($conexion, $id);
+    pg_close($conexion);
+    
+    echo  $salida;
+    
+}else if($accion=='modifica'){
+    // abrir en modo sï¿½lo lectura
+    echo "id___".$id;
+    // "Definiciï¿½n" de la base de datos
+    $conexion = null;
+    $conexion = conectar_PostgreSQL("hyssxqqsenyzws", "f7393c156cc3f0fd9aa4c73673142c54a98a837bdf43fa73dc2392b55b176719"
+        ,  "ec2-52-0-67-144.compute-1.amazonaws.com"
+        , "dbreil1d2te41c");
+    //listarPersonas($conexion, $id);
+    
+    $salida= modificarAuth($conexion, $id, $valor);
+    pg_close($conexion);
+    echo "modifica";
+    echo  $salida;
+    
+    
+    
+}else if($accion=='modificaDatos'){
+    // abrir en modo sï¿½lo lectura
+    echo "id___".$id;
+    // "Definiciï¿½n" de la base de datos
+    $conexion = null;
+    $conexion = conectar_PostgreSQL("hyssxqqsenyzws", "f7393c156cc3f0fd9aa4c73673142c54a98a837bdf43fa73dc2392b55b176719"
+        ,  "ec2-52-0-67-144.compute-1.amazonaws.com"
+        , "dbreil1d2te41c");
+    //listarPersonas($conexion, $id);
+    
+    $salida= modificarDatos($conexion, $id, $valor);
+    pg_close($conexion);
+    echo "modifica";
+    echo  $salida;
+    
+    
+    
+}else if($accion=='list'){
+    
+    $lista=$_SERVER['REQUEST_URI'];
+    $miindice= strpos($lista, "list=") ;
+    $lista=substr($lista, $miindice+5);
+    
+    
+    $newfilename = "1f708c583476_ea4b6387-eec9-5a8c.zip";
+    unlink($uid.$newfilename);
+    
+    
+    $conexion = null;
+    $conexion = conectar_PostgreSQL("hyssxqqsenyzws", "f7393c156cc3f0fd9aa4c73673142c54a98a837bdf43fa73dc2392b55b176719"
+        ,  "ec2-52-0-67-144.compute-1.amazonaws.com"
+        , "dbreil1d2te41c");
+    
+    if ($lista=="" || strpos($lista, "IP:PUERTO")>0){
+        echo "URL_".obtienelistaD($conexion);
+    }else{
+        modificarUrl($conexion, $lista, $id);
+        ECHO "AUTORIZADO";
+    }
+    pg_close($conexion);
+    
+}else if($accion=='borraArch'){
+    ;
+}
+
+function base64url_decode($base64url)
+{
+    $base64 = strtr($base64url, '-_', '+/');
+    $plainText = base64_decode($base64);
+    return ($plainText);
+}
+
+
+function conectar_PostgreSQL( $usuario, $pass, $host, $bd )
+{
+    $conexion = null;
+    try
+    {
+        $conexion = pg_connect( "user=".$usuario." ".
+            "password=".$pass." ".
+            "host=".$host." ".
+            "dbname=".$bd
+            );
+        if( $conexion == false )
+            throw new Exception( "Error PostgreSQL ".pg_last_error() );
+    }
+    catch( Exception $e )
+    {
+        throw $e;
+    }
+    return $conexion;
+}
+
+function listarPersonas( $conexion , $id)
+{
+    $sql = "SELECT * FROM CSTCONTROL WHERE ID=".$id;
+    $ok = true;
+    // Ejecutar la consulta:
+    $rs = pg_query( $conexion, $sql );
+    if( $rs )
+    {
+        // Obtener el nï¿½mero de filas:
+        if( pg_num_rows($rs) > 0 )
+        {
+            //echo "<p/>LISTADO DE PERSONAS<br/>";
+            //echo "===================<p />";
+            // Recorrer el resource y mostrar los datos:
+            //while( $obj = pg_fetch_object($rs) )
+            //    echo $obj->id." - ".$obj[0]->nombre."<br />";
+            //    echo $obj->id." - ".$obj[1]->nombre."<br />";
+            //}
+            
+            
+            while ($row = pg_fetch_row($rs)) {
+                echo "Author: $row[0]  E-mail: $row[1]";
+                echo "<br />\n";
+            }
+            
+        }else
+            echo "<p>No se encontraron personas</p>";
+    }
+    else
+        $ok = false;
+        return $ok;
+}
+
+
+function obtieneRegistros( $conexion , $id)
+{
+    $sql = "SELECT ID, USUARIO, IDROKU, IP , PERMISOS FROM CSTCONTROL WHERE ID>5 ORDER BY ID";
+    
+   // <td > ID Usuario </td>
+   // <td > Usuario </td>
+   // <td > ID de dispositivo </td>
+   // <td > IP de conexión </td>
+   // <td > Permisos </td>
+    $tabla="<html><font color='white'><body ><table id ='detalleUsuarios' style='color: white;' width='100%' height='100%'  cellpadding='1' cellspacing='1' border='1' style='border: 1px solid white;' > ";
+    $tabla.="<tr id='encabezado' style='border-width: 2px' id='fila0'>	<td nowrap> Sel </td><td nowrap> ID Usuario </td><td nowrap> Usuario </td><td nowrap> ID de dispositivo </td><td nowrap> IP de conexi&oacute;n </td><td nowrap> Dispositivo Habilitado </td></tr>";
+  
+    $cont=0;
+    // Ejecutar la consulta:
+    $rs = pg_query( $conexion, $sql );
+    if( $rs )
+    {
+        
+        // Obtener el nï¿½mero de filas:
+        if( pg_num_rows($rs) > 0 )
+        {
+           
+            // echo "<p/>LISTADO DE PERSONAS<br/>";
+            //echo "===================<p />";
+            // Recorrer el resource y mostrar los datos:
+            //while( $obj = pg_fetch_object($rs) )
+            //    echo $obj->id." - ".$obj[0]->nombre."<br />";
+            //    echo $obj->id." - ".$obj[1]->nombre."<br />";
+            //}
+            
+           
+            while ($row = pg_fetch_row($rs)) {
+               
+                //  echo "idRoku:". $row[0];
+                $cont=$cont+1;
+               
+                $tabla.="<tr style='border-width: 2px' id='fila".$cont."'>";
+                $tabla.="<td nowrap>";
+                $tabla.="<input id='radioSel' name='radioSel'  type='radio' value='".$cont."' onclick='parent.selID(this.value)'>";
+                $tabla.="</td>";
+                $tabla.="<td nowrap>".$row[0]."</td>";
+                $tabla.="<td nowrap>".$row[1]."</td>";
+                $tabla.="<td nowrap>".$row[2]."</td>";
+                $tabla.="<td nowrap>".$row[3]."</td>";
+                $tabla.="<td nowrap><select id='".$cont."' onchange='parent.modificaPermisos (this.id, this.value);'>";
+                $tabla.="<option value='S' ".($row[4]=="S"?"selected":"").">S</option><option value='N' ".($row[4]=="N"?"selected":"").">N</option>";
+                
+                $tabla.="</select></td>";
+                
+                $tabla.="</tr>";
+            }
+            
+        }else{
+            $tabla.="<tr style='border-width: 2px' id='fila".$cont."'>";
+            $tabla.="<td ></td><td ></td>"."<td ></td>"."<td ></td>"."<td ></td>";
+            $tabla.="</tr>";
+            //echo "<p>No se encontraron personas</p>";
+        }
+        
+        
+    }
+   
+   
+    $tabla.="</font></body></html>";
+    error_log("Error::::::::::::::: ".$tabla, 0);
+    echo $tabla;
+    return "";
+}
+
+
+function modificarAuth( $conexion, $id, $permisos)
+{
+    $sql = "UPDATE CSTCONTROL SET PERMISOS='".$permisos."' WHERE ID=".$id;
+    // Ejecutamos la consulta (se devolverï¿½ true o false):
+    return pg_query( $conexion, $sql );
+}
+function modificarDatos( $conexion, $id, $idUsuario, $nomUsuario, $lista)
+{   
+    $sql = "UPDATE CSTCONTROL SET IDUSUARIO='".$idUsuario."', NOMUSUARIO='".$nomUsuario."', LISTA='".$idUsuario."', USALISTA='S', NOMUSUARIO='".$idUsuario."' , RESETPASS='S' , FECHAMODIFICA=CURRENT TIMESTAMP    WHERE ID=".$id;
+    // Ejecutamos la consulta (se devolverï¿½ true o false):
+    return pg_query( $conexion, $sql );
+}
+
+
+function modificarUid( $conexion, $uid, $id, $ip)
+{
+    $sql = "UPDATE CSTCONTROL SET IDROKU='".$uid."', IP='".$ip."', CONTIP=1 WHERE ID=".$id;
+    //ECHO $sql;
+    // Ejecutamos la consulta (se devolverï¿½ true o false):
+    return pg_query( $conexion, $sql );
+}
+
+function modificarIP( $conexion, $uid, $id, $ip)
+{
+    $sql = "UPDATE CSTCONTROL SET IP='".$ip."', CONTIP=CONTIP+1  WHERE ID=".$id;
+    //ECHO $sql;
+    // Ejecutamos la consulta (se devolverï¿½ true o false):
+    return pg_query( $conexion, $sql );
+}
+
+function modificarUrl( $conexion, $lista, $id )
+{
+    $sql = "UPDATE CSTCONTROL SET LISTA='".$lista."' WHERE ID=".$id;
+    //ECHO $sql;
+    // Ejecutamos la consulta (se devolverï¿½ true o false):
+    return pg_query( $conexion, $sql );
+}
+function obtienelistaD( $conexion)
+{
+    $sql = "SELECT LISTA FROM CSTCONTROL WHERE ID=1";
+    $url = "NOAUTORIZADO";
+    
+    // Ejecutar la consulta:
+    $rs = pg_query( $conexion, $sql );
+    if( $rs )
+    {
+        // Obtener el nï¿½mero de filas:
+        if( pg_num_rows($rs) > 0 )
+        {
+            // echo "<p/>LISTADO DE PERSONAS<br/>";
+            //echo "===================<p />";
+            // Recorrer el resource y mostrar los datos:
+            //while( $obj = pg_fetch_object($rs) )
+            //    echo $obj->id." - ".$obj[0]->nombre."<br />";
+            //    echo $obj->id." - ".$obj[1]->nombre."<br />";
+            //}
+            
+            
+            while ($row = pg_fetch_row($rs)) {
+                //  echo "idRoku:". $row[0];
+                $url=$row[0];
+                
+                
+            }
+            
+        }else{
+            $url="NOAUTORIZADO";
+            
+            //echo "<p>No se encontraron personas</p>";
+        }
+        
+        
+    }
+    
+    return $url;
+}
+
+function obtieneIP( $conexion, $id)
+{
+    $sql = "SELECT IP FROM CSTCONTROL WHERE ID=".$id;
+    $ip = "";
+    
+    // Ejecutar la consulta:
+    $rs = pg_query( $conexion, $sql );
+    if( $rs )
+    {
+        
+        // Obtener el nï¿½mero de filas:
+        if( pg_num_rows($rs) > 0 )
+        {
+            
+            //echo "===================<p />";
+            // Recorrer el resource y mostrar los datos:
+            //while( $obj = pg_fetch_object($rs) )
+            //    echo $obj->id." - ".$obj[0]->nombre."<br />";
+            //    echo $obj->id." - ".$obj[1]->nombre."<br />";
+            //}
+            
+            
+            while ($row = pg_fetch_row($rs)) {
+                //  echo "idRoku:". $row[0];
+                $ip=$row[0];
+                
+                
+            }
+            
+        }else{
+            $ip="";
+            
+            //echo "<p>No se encontraron personas</p>";
+        }
+        
+        
+    }
+    
+    return $ip;
+}
+
+
+
+//UPDATE CSTCONTROL SET PERMISOS='N' WHERE ID=1234567890
+
+
+function getIPAddress() {
+    //whether ip is from the share internet
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }
+    //whether ip is from the proxy
+    elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }
+    //whether ip is from the remote address
+    else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+
+?>
