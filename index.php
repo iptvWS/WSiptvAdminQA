332867986eda4b7a18edd31247c1245f070d5703
iptvWS <88012543+iptvWS@users.noreply.github.com>
2021-08-26 09:53:06 -0700

    Add files via upload

--- index.php
+++ index.php
@@ -63,7 +63,7 @@ body {
 		window.open("utils.php?act=init", "frameDet");
 	}
 	function modificaDatos(btn){
-		if(btn="Aceptar"){
+		if(btn=="Aceptar"){
 			window.open("utils.php?act=modificaDatos&id="+document.frameDet.document.getElementById("fila"+valRadio).cells[1].innerHTML+"&nomUser="+document.getElementById("nomUsuario").value+"&usaLista=S"+"&idUsuario="+document.getElementById("idUsuario").value+"&list="+document.getElementById("lista").value, "frameDet");
 		}else{
 			
