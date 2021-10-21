ceb004db9ff11e7b32760d55b225e8b95deb6b8c
iptvWS <88012543+iptvWS@users.noreply.github.com>
2021-10-20 18:54:21 -0700

    Add files via upload

--- trataArch.php
+++ trataArch.php
@@ -25,6 +25,7 @@
             $linea=fgets($fh);
             
             $miindice= strpos($linea, "Function ") ;
+            echo "<br>$miindice ".$miindice;
             if($miindice>=0){
                 $grupo=substr($linea, $miindice+9);
                 $miindiceFin= strpos($grupo, "()") ;
