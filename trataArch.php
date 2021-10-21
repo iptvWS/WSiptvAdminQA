c03531b57a3369ec043951c8924c56f9396b5ade
iptvWS <88012543+iptvWS@users.noreply.github.com>
2021-10-20 19:29:14 -0700

    Add files via upload

--- trataArch.php
+++ trataArch.php
@@ -82,8 +82,8 @@
             //echo "_en whilw3";
             // check for empty indexes
             if (!empty($propiedad) AND !empty($valor)) {
-                
-                echo "<br> nINGUNO VACIO:________________________ ".(strpos($propiedad, "Title "));
+                echo "<br> nINGUNO VACIO:________________________ ".$propiedad;
+                echo "<br> nINGUNO VACIO:________________________ ".(strpos($propiedad, "Title"));
                 //echo "__propiedad".$propiedad;
                 //echo "valor".$valor;
                 // echo "_en whilw4";
